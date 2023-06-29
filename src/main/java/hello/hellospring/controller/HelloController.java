59fe71644d7e2724971c1bd8fbf02535ca0ad949
Song0-0 <ks08094@naver.com>
2023-06-29 12:31:19 +0900

    2. 정적컨텐츠, MVC와 템플릿 엔진, API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,44 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    /**
+     * json방식으로 나타내기
+     * return값이 그냥 문자가 아니라 객체이다.
+     * 객체가 오면 스프링에서는 json방식으로 데이터를 만들어서 http응답에 반환하겠다. 이게 기본 정책이다.
+     * @param name
+     * @return
+     */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
